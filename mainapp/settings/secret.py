66110a7d69a43a00c9dfdb14dae55fa835c8de7a
titovanton <mail@titovanton.com>
2014-07-29 19:21:49 +0600

    host

--- mainapp/settings/secret.py
+++ mainapp/settings/secret.py
@@ -4,6 +4,9 @@ U must add it to .gitignore file
 and set Unix file change mode and owner
 '''
 
+import os
+
+
 DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
@@ -18,15 +21,15 @@ DATABASES = {
         'NAME': '{{ project_name }}',
         'USER': '{{ project_name }}',
         'PASSWORD': '%DB_PWD%',
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': os.environ['DB_PORT_5432_TCP_ADDR'],
+        'PORT': 5432,
     }
 }
 
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': 'localhost:11211',
+        'LOCATION': os.environ['DB_PORT_11211_TCP_ADDR'] + ':11211',
     }
 }
 
@@ -37,7 +40,7 @@ CACHE_TIMEOUT = 60 * 60 * 24 * 30 * 12
 KEY_PREFIX = '{{ project_name }}'
 
 REDIS_CONNECTION = {
-    'host': 'localhost',
+    'host': os.environ['DB_PORT_6379_TCP_ADDR'],
     'port': 6379,
     'db': 1,
 }
